3672fe15b4cafb403d4c6550807197135723ef74
AleksandrRom <alex32006@mail.ru>
2025-07-03 00:16:14 +0300

    03-06-25_01

--- task_45_Find_Fist_and_Last_Position_of_Element_in_Sorted_Array/main.cpp
+++ task_45_Find_Fist_and_Last_Position_of_Element_in_Sorted_Array/main.cpp
@@ -1,45 +1,81 @@
-#include <vector>
 #include <cassert>
-
+#include <vector>
 
 class Solution {
-public:
+   public:
     std::vector<int> searchRange(std::vector<int>& nums, int target) {
-        auto it_left = nums.begin();
-        auto it_right = nums.end();
-        std::vector<int> result = {-1,-1};
-        int left = 0;
-        int right = 0;
-        auto pos = it_left;
+        if (nums.empty()) {
+            return {-1, -1};
+        }
+        auto it_left_start = nums.begin();
+        auto it_left_end = nums.end();
+        auto it_right_start = nums.begin();
+        auto it_right_end = nums.end();
+        // std::vector<int> result = {-1, -1};
+        // int left = 0;
+        // int right = 0;
+        auto pos_l = it_left_start;
+        auto pos_r = it_right_end;
         while (true) {
-            pos = it_left + (it_right - it_left) / 2;
-            if (*pos < target) {
-                it_left = pos;
-            } else if (*pos > target) {
-                it_right = pos;
+            pos_l = it_left_start + (it_left_end - it_left_start) / 2;
+            pos_r = it_right_start + (it_right_end - it_right_start) / 2;
+            auto left = pos_l - nums.begin();
+            auto right = pos_r - nums.begin();
+            if (it_left_end - it_left_start < 2 && it_right_end - it_right_start < 2) {
+                if (*pos_r != target) {
+                    return {-1, -1};
+                }
+                int n = (*pos_l == target ? 0 : 1);
+                return {static_cast<int>(pos_l - nums.begin()) + n, static_cast<int>(pos_r - nums.begin())};
+            }
+            ///////////////////////
+            if (*pos_l < target) {
+                it_left_start = pos_l;
+            } else if (*pos_l >= target) {
+                it_left_end = pos_l;
+            }
+            ///////////////////////
+            if (*pos_r <= target) {
+                it_right_start = pos_r;
+            } else if (*pos_r > target) {
+                it_right_end = pos_r;
             }
         }
-        
     }
 };
 
-
-int main () {
+int main() {
     Solution s;
     {
-        std::vector<int> v{0,0,0,1,1,1,2,3,5,7,7,8,8,8,8,8,8,10};
-        assert((std::vector<int>{3,4} == s.searchRange(v, 8)));
+        std::vector<int> v{1, 1, 2};
+        assert((std::vector<int>{0, 1} == s.searchRange(v, 1)));
     }
     {
-        std::vector<int> v{5,7,7,8,8,10};
-        assert((std::vector<int>{3,4} == s.searchRange(v, 8)));
+        std::vector<int> v{0, 0, 0, 1, 1, 1, 2, 3, 5, 7, 7, 8, 8, 8, 8, 8, 8};
+        assert((std::vector<int>{0, 2} == s.searchRange(v, 0)));
     }
     {
-        std::vector<int> v{5,7,7,8,8,10};
-        assert((std::vector<int>{-1,-1} == s.searchRange(v, 6)));
+        std::vector<int> v{5, 7, 7, 8, 8, 10};
+        assert((std::vector<int>{-1, -1} == s.searchRange(v, 6)));
+    }
+    {
+        std::vector<int> v{5, 7, 8, 8, 10};
+        assert((std::vector<int>{1, 1} == s.searchRange(v, 7)));
+    }
+    {
+        std::vector<int> v{0, 0, 0, 1, 1, 1, 2, 3, 5, 7, 7, 8, 8, 8, 8, 8, 8, 10};
+        assert((std::vector<int>{11, 16} == s.searchRange(v, 8)));
+    }
+    {
+        std::vector<int> v{5, 7, 7, 8, 8, 10};
+        assert((std::vector<int>{3, 4} == s.searchRange(v, 8)));
     }
     {
         std::vector<int> v{};
-        assert((std::vector<int>{-1,-1} == s.searchRange(v, 0)));
+        assert((std::vector<int>{-1, -1} == s.searchRange(v, 0)));
+    }
+    {
+        std::vector<int> v{0, 1, 2, 8, 8, 8, 8, 8, 8};
+        assert((std::vector<int>{3, 8} == s.searchRange(v, 8)));
     }
 }
---
+++ task_47_Sudoku_solover/main.cpp
@@ -0,0 +1,344 @@
+#include <cassert>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <numeric>
+
+using namespace std::literals;
+
+class CheckSudoku {
+public:
+    bool CheckPosition(int i, int j) {
+        if (i == 1 || i == 4 || i == 7) {
+            return (j == 1 || j == 4 || j == 7);
+        }
+        return false;
+    }
+    bool isValidSudoku(std::vector<std::vector<char>>& board) {
+        for (int i = 0; i < 9; ++i) {
+            std::unordered_map<char, int> row;
+            std::unordered_map<char, int> collum;
+            for (int j = 0; j < 9; ++j) {
+                if (row.count(board[i][j]) || collum.count(board[j][i])) {
+                    return false;
+                }else {
+                    if (board[i][j] != '.') {
+                        ++row[board[i][j]];
+                    }
+                    if (board[j][i] != '.') {
+                        ++collum[board[j][i]];
+                    }
+                }
+                        // check square
+            if (CheckPosition(i, j)) {
+                    std::unordered_map<char, int> square;
+                    for (int n = -1; n < 2; ++n) {
+                        for (int k = -1; k < 2; ++k) {
+                            if (square.count(board[i + n][j + k])) {
+                                return false;
+                            }else {
+                                if (board[i + n][j + k] != '.') {
+                                    ++square[board[i + n][j + k]];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+// class Cell {
+
+
+//     private:
+//     std::vector<std::vector<char>>& board;
+
+// };
+
+class Solution {
+public:
+    void PrintBoard(std::vector<std::vector<char>>& board) {
+        std::cout << "----------------------" << std::endl;
+        for (int row = 0; row < 9; ++row) {
+            if (row == 3 || row == 6) {
+                std::cout << "----------------------" << std::endl;
+            }
+            for (int colum = 0; colum < 9; ++colum) {
+                if (colum == 3 || colum == 6 || colum == 0) {
+                    std::cout << '|';
+                } 
+                std::cout  << board[row][colum] << ' ' ;
+            }
+            std::cout << '|' << std::endl;
+            }
+            std::cout << "----------------------" << std::endl;
+            std::cout << std::endl;
+
+    }
+    std::vector<char> GetBox(std::vector<std::vector<char>>& board, int row, int colum) {
+        int min_row = (row / 3) * 3;
+        int min_colum = (colum / 3) * 3;
+        std::vector<char> result;
+        result.reserve(8);
+        for (int i = min_row; i < min_row + 3; ++i) {
+            for (int j = min_colum; j < min_colum + 3; ++j) {
+                    result.push_back(board[i][j]);
+            }
+        }
+        return result;
+    }
+
+    std::vector<char> InsertInt(std::vector<std::vector<char>>& board, int row, int colum) {
+        std::unordered_set<char> available_number{'1','2','3','4','5','6','7','8','9'};
+        std::vector<char> box_n = GetBox(board, row, colum);
+        for (int i = 0; i < 9; ++i){
+            if (board[row][i] != '.') {
+                available_number.erase(board[row][i]);
+            }
+            if (board[i][colum] != '.') {
+                available_number.erase(board[i][colum]);
+            }
+            if (box_n[i] != '.') {
+                available_number.erase(box_n[i]);
+            }
+        }
+        return {available_number.begin(), available_number.end()};
+
+    }
+
+    void InsertBox(std::vector<std::vector<char>>& board, int row, int colum) {
+        int row_min = (row / 3) * 3;
+        int colum_min = (colum / 3) * 3;
+        for (char number = '1'; number <= '9'; ++number) {// проверка каждого числа
+            std::vector<char> box_index(9, number); //индексы в боксе
+
+            for (int i = row_min; i < row_min + 3; ++i) {
+                if (CheckNumberInRow(board, i, number)) {
+                    int row_min = (i % 3) * 3;
+                    for (int r = row_min; r < row_min + 3; ++r) {
+                        box_index[r] = '.';
+                    }
+                }
+            }
+
+            for (int j = colum_min; j < colum_min + 3; ++j) {
+                    if (CheckNumberInColum(board, j, number)) {
+                        int colum_min = (j % 3);
+                        for (int c = colum_min; c < colum_min + 7; c += 3) {
+                            box_index[c] = '.';
+                        }
+                    }
+            }
+
+            auto box = GetBox(board, row, colum);
+            for (int i = 0; i < 9; ++i) {
+                if (box[i] == number) {
+                    box_index.resize(0);
+                    break;
+                    // box_index[i] = '.';
+                }
+            }
+
+            //подсчет количеста
+            if (box_index.size() > 0) {
+            int pos = PositionIsCountNumber(box_index);
+                if (pos >= 0) {
+                    board[row + pos / 3][colum + (pos % 3)] = number;
+                    PrintBoard(board);
+                    int hhh = 0;
+                }
+            }
+            }
+        }
+
+        int PositionIsCountNumber (std::vector<char>& box_index) {
+            int position = -1;
+            bool first = true;
+            for (int i = 0; i < 9; ++i) {
+                if (box_index[i] != '.') {
+                    position = i;
+                    if (!first) return -1;
+                    first = false;
+                }
+            }
+            return position;
+        }
+
+    // }
+    // }
+    bool CheckSolover(std::vector<std::vector<char>>& board) {
+        for (int row = 0; row < 9; ++row) {
+            for (int colum = 0; colum < 9; ++colum) {
+                if (board[row][colum] == '.') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(std::vector<std::vector<char>>& board) {
+        PrintBoard(board);
+        int iter = 1;
+        while(true) {
+            for (int row = 0; row < 9; ++row) {
+                for (int colum = 0; colum < 9; ++colum) {
+                    if (board[row][colum] == '.') {
+                        auto temp = InsertInt(board, row, colum);
+                        if (temp.size() == 1) {
+                            //найдена ячейка для вставки
+                            board[row][colum] = temp.back();
+                            std::cout << "iteration: " << iter++ << std::endl;
+                            std::cout << "row: " << row << std::endl;
+                            std::cout << "colum: " << colum << std::endl;
+                            std::cout << "value: " << temp.back() << std::endl;
+                            PrintBoard(board);
+                            row = 0;
+                            colum = 0;
+                        }
+                    }
+                }
+            }
+            if (CheckSolover(board)) return;
+
+            for (int i = 0; i < 9; i += 3) {
+                for (int j = 0; j < 9; j += 3) {
+                    InsertBox(board, i, j);
+                }
+            }
+        }
+        
+    }
+    private:
+    bool CheckNumberInRow(std::vector<std::vector<char>>& board, int row, char number) {
+        for (int i = 0; i < 9; ++i) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+    bool CheckNumberInColum(std::vector<std::vector<char>>& board, int colum, char number) {
+        for (int i = 0; i < 9; ++i) {
+            if (board[i][colum] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // bool CheckNumberInBox(std::vector<std::vector<char>>& board, int row, int colum, char number) {
+    //     auto box = GetBox(board, row, colum);
+    //     for (const auto& num : box) {
+    //         if (num == number) return true;
+    //     }
+    //     return false;
+    // }
+    // std::vector<std::unordered_set<char>> row_;
+    // std::vector<std::unordered_set<char>> colum_;
+};
+
+
+int main () {
+Solution s;
+{
+    std::vector<std::vector<char>> v
+{{'.','.','9','7','4','8','.','.','.'}
+,{'7','.','.','.','.','.','.','.','.'}
+,{'.','2','.','1','.','9','.','.','.'}
+,{'.','.','7','.','.','.','2','4','.'}
+,{'.','6','4','.','1','.','5','9','.'}
+,{'.','9','8','.','.','.','3','.','.'}
+,{'.','.','.','8','.','3','.','2','.'}
+,{'.','.','.','.','.','.','.','.','6'}
+,{'.','.','.','2','7','5','9','.','.'}};
+s.solveSudoku(v);
+}
+// {
+//     std::vector<std::vector<char>> v
+// {{'5','3','.','.','7','.','.','.','.'}
+// ,{'6','.','.','1','9','5','.','.','.'}
+// ,{'.','9','8','.','.','.','.','6','.'}
+// ,{'8','.','.','.','6','.','.','.','3'}
+// ,{'4','.','.','8','.','3','.','.','1'}
+// ,{'7','.','.','.','2','.','.','.','6'}
+// ,{'.','6','.','.','.','.','2','8','.'}
+// ,{'.','.','.','4','1','9','.','.','5'}
+// ,{'.','.','.','.','8','.','.','7','9'}};
+// s.solveSudoku(v);
+// }
+
+int i = 0;
+/*
+{
+    std::vector<std::vector<char>> v 
+{{'.','.','.','9','.','.','.','.','.'}
+,{'.','.','.','.','.','.','.','.','.'}
+,{'.','.','3','.','.','.','.','.','1'}
+,{'.','.','.','.','.','.','.','.','.'}
+,{'1','.','.','.','.','.','3','.','.'}
+,{'.','.','.','.','2','.','6','.','.'}
+,{'.','9','.','.','.','.','.','7','.'}
+,{'.','.','.','.','.','.','.','.','.'}
+,{'8','.','.','8','.','.','.','.','.'}};
+ assert(false == s.isValidSudoku(v));
+}
+{
+    std::vector<std::vector<char>> v 
+{{'.','.','.','9','.','.','.','.','1'}
+,{'.','.','.','.','.','.','.','.','.'}
+,{'.','.','3','.','.','.','.','.','1'}
+,{'.','.','.','.','.','.','.','.','.'}
+,{'1','.','.','.','.','.','3','.','.'}
+,{'.','.','.','.','2','.','6','.','.'}
+,{'.','9','.','.','.','.','.','7','.'}
+,{'.','.','.','.','.','.','.','.','.'}
+,{'8','.','.','8','.','.','.','.','.'}};
+ assert(false == s.isValidSudoku(v));
+}
+ */
+// {
+//     std::vector<std::vector<char>> v 
+// {{'5','3','.','.','7','.','.','.','.'}
+// ,{'6','.','.','1','9','5','.','.','.'}
+// ,{'.','9','8','.','.','.','.','6','.'}
+// ,{'8','.','.','.','6','.','.','.','3'}
+// ,{'4','.','.','8','.','3','.','.','1'}
+// ,{'7','.','.','.','2','.','.','.','6'}
+// ,{'.','6','.','.','.','.','2','8','.'}
+// ,{'.','.','.','4','1','9','.','.','5'}
+// ,{'.','.','.','.','8','.','.','7','9'}};
+//  assert(true == s.isValidSudoku(v));
+// }
+// {
+//     std::vector<std::vector<char>> v 
+// {{'8','3','.','.','7','.','.','.','.'}
+// ,{'6','.','.','1','9','5','.','.','.'}
+// ,{'.','9','8','.','.','.','.','6','.'}
+// ,{'8','.','.','.','6','.','.','.','3'}
+// ,{'4','.','.','8','.','3','.','.','1'}
+// ,{'7','.','.','.','2','.','.','.','6'}
+// ,{'.','6','.','.','.','.','2','8','.'}
+// ,{'.','.','.','4','1','9','.','.','5'}
+// ,{'.','.','.','.','8','.','.','7','9'}};
+//  assert(false == s.isValidSudoku(v));
+// }
+// {
+//     std::vector<std::vector<char>> v 
+// {{'.','.','.','.','.','.','.','.','.'}
+// ,{'.','.','.','.','.','.','.','.','.'}
+// ,{'.','.','.','.','.','.','.','.','.'}
+// ,{'.','.','.','.','.','.','.','.','.'}
+// ,{'.','.','.','.','.','.','.','.','.'}
+// ,{'.','.','.','.','.','.','.','.','.'}
+// ,{'.','.','.','.','.','.','3','.','.'}
+// ,{'.','.','.','.','.','.','.','.','.'}
+// ,{'.','.','.','.','.','.','.','.','3'}};
+//  assert(false == s.isValidSudoku(v));
+// }
+}

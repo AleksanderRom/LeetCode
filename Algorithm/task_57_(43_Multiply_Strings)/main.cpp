086e8b908dc090c44f408f9b25c4c8a86eae78c0
AleksandrRom <alex32006@mail.ru>
2025-11-11 19:17:09 +0300

    added solution task 57

--- Algorithm/task_57_(43_Multiply_Strings)/main.cpp
+++ Algorithm/task_57_(43_Multiply_Strings)/main.cpp
@@ -7,21 +7,27 @@ using namespace std;
 
 class Solution {
    public:
-    // int StringToInterger(const string& num) {
-    //     int buf = 0;
-    //     int n = num.size();
-    //     for (int i = 0; i < n; ++i) {
-    //         buf *= 10;
-    //         buf += (num[i] - 48);
-    //     }
-    //     return buf;
-    // }
-
     int ChToInt(char ch) {
         return ch - 48;
     }
 
+    void Inizializate(vector<vector<int>>& dp, string& num1, string& num2){
+        int n1 = num1.size();
+        int n2 = num2.size();
+        for (int i = 0; i < n2; ++i) {
+            for (int j = 0; j < n1; ++j) {
+                auto in1 = ChToInt(num1[n1 - j - 1]);
+                auto in2 = ChToInt(num2[n2 - i - 1]);
+                dp[i][j] = in1 * in2;
+            }
+        }
+    }
+
     string multiply(string num1, string num2) {
+        if (num1[0] == '0' || num2[0] == '0') {
+            return "0";
+        }
+
         int n1 = num1.size();
         int n2 = num2.size();
         if (n2 > n1) {
@@ -29,21 +35,14 @@ class Solution {
             std::swap(n1, n2);
         }
         vector<vector<int>> dp(n2, vector<int>(n1));
-
-        for (int i = 0; i < n2; ++i) {
-            for (int j = 0; j < n1; ++j) {
-                auto in1 = ChToInt(num1[n1 - i - 1]);
-                auto in2 = ChToInt(num2[n2 - j - 1]);
-                dp[i][j] = in1 * in2;
-            }
-        }
+        Inizializate(dp, num1, num2);
 
         string result;
         for (int i = 0; i < n1 + n2 - 1; ++i) {
             int buf = 0;
 
             for (int j = 0, k = i; j < i + 1; ++j, --k) {
-                if (j >= n1 || k >= n2) {
+                if (j >= n2 || k >= n1) {
                     continue;
                 }
                 buf += dp[j][k];
@@ -83,15 +82,17 @@ int main() {
 void Test() {
     Solution s;
     assert("492" == s.multiply("123", "4"));
+    assert("0" == s.multiply("4158", "0"));
     auto ddd = s.multiply("123456789", "987654321");
     assert("121932631112635269"s == s.multiply("123456789", "987654321"));
-    // assert(32 == s.StringToInterger("32"));
-    // assert(132 == s.StringToInterger("132"));
-    // assert(0 == s.StringToInterger("0"));
-    // assert(0 == s.StringToInterger(""));
+
 
     auto t = s.multiply("2", "3");
     assert("6" == s.multiply("2", "3"));
     assert("56088" == s.multiply("123", "456"));
     assert("492" == s.multiply("4", "123"));
+    assert("0" == s.multiply("4", "0"));
+    assert("0" == s.multiply("4158", "0"));
+    assert("0" == s.multiply("0", "1586"));
+    assert("998999999001" == s.multiply("999", "999999999"));
 }
